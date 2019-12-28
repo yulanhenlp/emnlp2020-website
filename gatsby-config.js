3ebb96d50a88263db6035040f9302f1a2afeb71b
Andy MacKinlay <am@akmy.net>
2019-12-28 15:46:24 +0800

    updated config to get data

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,13 @@ module.exports = {
         plugins: [],
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+      },
+    },
     "gatsby-transformer-csv",
     {
       resolve: "gatsby-plugin-google-analytics",
