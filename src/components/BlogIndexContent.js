b5f8e45bab46194b7728247838f0ac33410c7993
Andy MacKinlay <am@akmy.net>
2020-04-25 14:16:02 +1000

    show updates on blog post listing

--- src/components/BlogIndexContent.js
+++ src/components/BlogIndexContent.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "gatsby";
 
-const BlogIndexListing = ({ post }) => (
-  <li>
-    <Link to={post.fields.slug}>{post.frontmatter.date}: {post.frontmatter.title}</Link>
-  </li>
-);
+const BlogIndexListing = ({ post }) => {
+  const { title, date, updated } = post.frontmatter;
+  const { slug } = post.fields
+  return (<li>
+    <Link to={slug}>{date}: {title}</Link>
+    {(updated && updated.length > 0) ? <span className="blog-updated"> (Updated: {updated[0].date})</span> : null}
+  </li>);
+};
 
 const BlogIndexContent = ({ posts }) => (
   <article className="blog-posts default-content">
